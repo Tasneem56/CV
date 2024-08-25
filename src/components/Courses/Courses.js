a2b347783408d14563b58d83df3425942d771022
Tasneem <tassnemm70@gmail.com>
2024-08-25 15:48:20 +0300

    tasneem

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -10,19 +10,19 @@ function Courses() {
                         <h2 style={{ color: `var(--main-color)` }}>Courses : </h2>
                         <p className={style.p}>
                             <h4 style={{ color: `var(--secondary-color)` }}>●	React.js</h4>
-                            - Online Course <span style={{ color: `#777` }}>Jun 2024</span><br />
+                            - Online Course <span style={{ color: `#808080` }}>Jun 2024</span><br />
                             - (JS revision – React basics – React Hooks – Redux
                             toolkit)
                         </p>
                         <p className={style.p}>
                             <h4 style={{ color: `var(--secondary-color)` }}>●	Frontend Web Development</h4>
-                            - Information Technology Institute (ITI) <span style={{ color: `#777` }}>Jul 2023 - Sep 2023</span><br />
+                            - Information Technology Institute (ITI) <span style={{ color: `#808080` }}>Jul 2023 - Sep 2023</span><br />
                             - (HTML - CSS - JAVASCRIPT - ES6 - Bootstrap5 - React.js)
                         </p>
                         <p className={style.p}>
                             <h4 style={{ color: `var(--secondary-color)` }}> ●	Web development basics
                             </h4>
-                            - Online Course <span style={{ color: `#777` }}>Feb 2023</span><br />
+                            - Online Course <span style={{ color: `#808080` }}>Feb 2023</span><br />
                             - (HTML5 - CSS3 - JS )
                         </p>
                     </div>
