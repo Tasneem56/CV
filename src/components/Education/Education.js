cda1d3bb75a1827fd6c1dfbf304e9e94ab3db8a2
Tasneem <tassnemm70@gmail.com>
2024-08-25 15:51:19 +0300

    tasneem

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -7,11 +7,11 @@ function Education() {
             <h2 style={{ color: `var(--main-color)` }}>Education : </h2>
                 <div className='col-sm-12 col-md-6'>
                     <p>
-                        ●	Have a Bachelor’s degree in Computer science from faculty of Computers and Information Menoufia University, Shebin El-Kom. Oct 2021 - Jul 2024
+                        ●	Have a Bachelor’s degree in Computer science from faculty of Computers and Information Menoufia University, Shebin El-Kom. <p style={{ color: `#808080` }}>Oct 2021 - Jul 2024</p>
                     </p>
                     <p>
-                        ●	Relevant Coursework :
-                        Computer Fundamentals, Web Development, Web Design, Database Management Systems, Operating System ,Object - Oriented Programming , Data Structure , Java script essential training , Java script advanced training , Node Framework, Distributed Systems, Embedded Systems.
+                        <p style={{ color: `var(--secondary-color)` }}>●	Relevant Coursework : </p>
+                        [Computer Fundamentals, Web Development, Web Design, Database Management Systems, Operating System ,Object - Oriented Programming , Data Structure , Java script essential training , Java script advanced training , Node Framework, Distributed Systems, Embedded Systems.]
                     </p>
                 </div>
                 <div className='col-sm-12 col-md-6'>
